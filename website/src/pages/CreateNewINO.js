e4dba0bcfaf50870d6c0957f424477cb7a2b2a7e
utkukoca <49529559+utkukoca@users.noreply.github.com>
2023-08-13 01:06:36 +0300

    creatnewino ui

--- website/src/pages/CreateNewINO.js
+++ website/src/pages/CreateNewINO.js
@@ -7,6 +7,8 @@ import { useAccount } from 'wagmi';
 import { useNavigate } from 'react-router-dom';
 import { storeFile, getFile, storeUri } from '../utils/getWeb3';
 
+import DatePicker from 'react-datepicker';
+
 const CreateNewINO = () => {
   const [name, setName] = useState(''); //sets name
   const [description, setDescription] = useState('');
@@ -95,24 +97,26 @@ const CreateNewINO = () => {
 
 
   return (
+    <div className="h-screen bg-black">
+
     <section className=" flex  items-center justify-center">
-      <div className=" mt-20  items-center overflow-hidden shadow-2xl border-2 border-gray-100 rounded-xl mb-5">
-        <div className="p-10">
-          <div className="border-b w-full">
-            <h1 className="text-xl font-bold text-center pb-2">Create New INO</h1>
+      <div className="  mt-20 items-center overflow-hidden shadow-2xl rounded-xl ">
+        <div className="bg-gray-800  p-10 mt-10 border  border-[#7316ff] rounded-xl">
+          <div className=" w-full">
+            <h1 className="text-xl font-bold text-center text-white pb-2">Create New INO</h1>
           </div>
-          <p className="pt-3">Name of the Project</p>
+          <p className="pt-3 text-white">Name of the Project</p>
           <input
             type="text"
-            className="block p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block mt-1 p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Project's Name"
             required
             onChange={(e) => handleName(e)}
           />
-          <p className="pt-3">Description</p>
+          <p className="pt-3 text-white">Description</p>
           <input
             type="text"
-            className="block p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block p-2 mt-1 text-white w-full text-sm  bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Project's description"
             required
             onChange={(e) => handleDescription(e)}
@@ -127,31 +131,31 @@ const CreateNewINO = () => {
                 src={image} />
               </div>
             ) : (
-              <p>Please select a file</p>
+              <p className='text-white'>Please select a file</p>
             )}
           </div>
-          <p className="pt-3">Giveaway Time</p>
+          <p className="pt-3 text-white">Giveaway Time</p>
           <input
             type="datetime-local"
             id="meeting-time"
-            className="block p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block mt-1 p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             label="Giveaway deadline"
             required
             onChange={(e) => handleGiveawayTime(e)}
           />
-          <p className="pt-3">NFT Contract Address</p>
+          <p className="pt-3 text-white">NFT Contract Address</p>
           <input
             type="text"
-            className="block p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block mt-1 p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Contract Address"
             required
             onChange={(e) => handleContract(e)}
           />
-          <p className="pt-2 font">NFT Price (in ETH)</p>
+          <p className="pt-2 text-white">NFT Price (in ETH)</p>
 
           <input
             type="text"
-            className="block p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block mt-1 p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Price in ETH"
             required
             onChange={(e) => handlePrice(e)}
@@ -166,6 +170,7 @@ const CreateNewINO = () => {
         </div>
       </div>
     </section>
+    </div>
   );
 };
 
