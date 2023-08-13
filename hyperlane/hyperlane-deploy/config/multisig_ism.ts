a32e909e51e30426e69b86e2259a61947e1fd97a
Kaanayden <kaanayden15@gmail.com>
2023-08-13 13:03:15 +0300

    Add hyperlance deploy config and artifacts

---
+++ hyperlane/hyperlane-deploy/config/multisig_ism.ts
@@ -0,0 +1,26 @@
+import { ChainMap, ModuleType, MultisigIsmConfig } from '@hyperlane-xyz/sdk';
+
+export const multisigIsmConfig: ChainMap<MultisigIsmConfig> = {
+  // ----------- Your chains here -----------------
+  anvil1: {
+    type: ModuleType.LEGACY_MULTISIG,
+    threshold: 1,
+    validators: [
+      // Last anvil address
+      '0xa0ee7a142d267c1f36714e4a8f75612f20a79720',
+    ],
+  },
+  anvil2: {
+    type: ModuleType.LEGACY_MULTISIG,
+    threshold: 1,
+    validators: [
+      // Last anvil address
+      '0xa0ee7a142d267c1f36714e4a8f75612f20a79720',
+    ],
+  },
+  zora: {
+    type: ModuleType.LEGACY_MULTISIG,
+    threshold: 1,
+    validators: ["0x78271569468F85f23Bd18242e182C8B9ed7061bB"]
+  }
+};
