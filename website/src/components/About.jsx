2c7b5dd77b8d97b4d0ce189e8f90f1ca2fc0fd4c
utkukoca <49529559+utkukoca@users.noreply.github.com>
2023-08-13 14:56:53 +0300

    About and About card content uptade

--- website/src/components/About.jsx
+++ website/src/components/About.jsx
@@ -11,18 +11,24 @@ const About = () => {
       <div className="flex mb-20 flex-wrap justify-center gap-3 w-full max-w-7xl mx-auto px-4 items-center">
         <AboutCard
           logo="./images/worldcoin.png"
-          title="Use WorldCoin"
-          subTitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          title="WorldCoin"
+          subTitle="It is important to protect against Sybil attacks in giveaways, and that's why we use WorldCoin. Thanks to WorldCoin, the same person cannot participate in NFT giveaways multiple times."
         />
         <AboutCard
           logo="./images/zora.png"
-          title="Join INO"
-          subTitle="Praesent pulvinar dolor sed turpis elementum, ac viverra est maximus."
+          title="Zora"
+          subTitle="PerCapita's main contracts have been deployed on the Zora blockchain, enabling all transactions within PerCapita to be conducted rapidly, affordably, and securely."
         />
         <AboutCard
+        
           logo="./images/hyperline.png"
-          title="Create Your Own INO"
-          subTitle="Integer interdum nisi nec neque interdum, in eleifend arcu faucibus."
+          title="Hyperlane"
+          subTitle="In order to safeguard our application from Sybil attacks, we wanted to utilize WorldCoin, and for fair selection, we aimed to use ChainLink VRF. However, this Dapp was not available on Zora. Fortunately, thanks to Hyperlane, we were able to acquire these Dapps from other chains and integrate them into Zora."
+        />
+        <AboutCard
+          logo="./images/chainlink.png"
+          title="ChainLink"
+          subTitle="PerCapita utilizes Chainlink's VRF system to ensure fair and secure lotteries for NFT giveaways."
         />
       </div>
     </div>
